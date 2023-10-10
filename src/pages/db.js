3072a999094ba7212b6c31108dd90e50df90666f
fatGubari <fatimajubary@gmail.com>
2023-10-10 20:20:18 +0300

    modify: delete unnessasery file

--- src/pages/db.js
+++ src/pages/db.js
@@ -1,11 +1,11 @@
-const { Pool} = require('pg')
+// const { Pool} = require('pg')
 
-const pool = new Pool({
-  user: 'fish_user',
-  database: 'fish',
-  password: 'password',
-  port: 5432,
-  host: 'localhost',
-})
+// const pool = new Pool({
+//   user: 'fish_user',
+//   database: 'fish',
+//   password: 'password',
+//   port: 5432,
+//   host: 'localhost',
+// })
 
-module.exports = { pool };
+// module.exports = { pool };
